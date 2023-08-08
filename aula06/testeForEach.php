fdb24fe4006e6287ebba723f039f7089a41a3471
Jefferson Teixeira <jefferson.teixeira@dataprev.gov.br>
2023-08-08 20:41:06 -0300

    AULA 06

--- aula06/testeForEach.php
+++ aula06/testeForEach.php
@@ -2,6 +2,17 @@
 
 $array = ['Jefferson', 'João', 'Marta', 'Marcus'];
 
+$array2 = ['Jefferson', 1, 'João', 2, 'Marta', 3, 'Marcus'];
+
+$y = 0;
+while ( $y < count($array2) ) {
+    
+    if (gettype($array2[$y])=='string') {
+        echo " $array2[$y] <br>";
+    }
+    $y++;
+}
+
 for ($i = 0 ; $i < count($array) ; $i++){
     echo "O elemento atual é o $array[$i] <br>";
 }
