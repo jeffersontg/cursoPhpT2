87b2bea3dfd2db652c8a7e3753431dcacd4d150c
Jefferson Teixeira <jefferson.teixeira@dataprev.gov.br>
2023-08-08 19:28:17 -0300

    AULA 06

---
+++ aula06/testeFor.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ordem crescente
+for ($i = 0; $i <= 10 ; $i++){
+  echo "O valor da variável é $i <br>"; 
+}
+
+// Ordem decrescente
+for ($i = 10; $i >= 0 ; $i--){
+
+    echo "O valor da variável é $i <br>"; 
+    
+    
+}
+?>
+
