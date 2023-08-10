5be12f98d9d5a40bbe8ac9c573f6e38ff2fd33a8
Jefferson Teixeira <jefferson.teixeira@dataprev.gov.br>
2023-08-10 20:38:25 -0300

    AULA 07

---
+++ aula07/testeHtml.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>Esse é um exemplo de H1</h1>  
+   <h2>Esse é um exemplo de H2</h2>  
+   <h3>Esse é um exemplo de H3</h3>  
+   <a href="testeString.php">Link para o testeString.php</a>
+
+   <h2> Exemplo de Table </h2>
+    <table>
+        <tr>
+            <th>Nome do aluno(a)</th>
+            <th>Presença em 20/06/2023</th>
+        </tr>
+        <tr>
+            <td>Estevam</td>
+            <td>Sim</td>
+        </tr>
+        <tr>
+            <td>Samantha</td>
+            <td>Sim</td>
+        </tr>
+        <tr>
+            <td>Duda</td>
+            <td>Sim</td>
+        </tr>
+        <tr>
+            <td>Vitor</td>
+            <td>Sim</td>
+        </tr>
+        <tr>
+            <td>Bianca</td>
+            <td>Sim</td>
+        </tr>
+
+
+    </table>
+</body>
+</html>
---
+++ aula07/testeIsabel.php
@@ -0,0 +1,16 @@
+<?php
+$variavel = "Jefferson";
+print("Meu nome é $variavel <br>");
+echo "<br>";
+printf("Meu nome é %d e o sobrenome é %s <br>", 2, "teste teste");
+
+$str = "Jefferson Teixeira";
+for ($i=0; $i < strlen(trim($str)); $i++) {
+    print("A letra da vez é " . substr(trim($str),$i,1)) . "<br>";
+}
+
+$str2 = "Banana, abacaxi, macaúba";
+$arrayString = explode(",", $str2);
+print_r($arrayString);
+
+?>
